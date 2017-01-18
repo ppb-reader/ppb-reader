54be00a492db6d5b22542eed5cb0d6095fd64cd7
1650440357ppb <1650440357@qq.com>
2017-01-19 02:16:15 +0800

    ppb

--- app.js
+++ app.js
@@ -57,15 +57,15 @@ app.use("/adminlogin",adminlogin);
 // });
 
 // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-//
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+ });
 
 app.listen(8088);
 // module.exports = app;
